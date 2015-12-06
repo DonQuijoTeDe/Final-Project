6f698de61c460dfb88bda1a5b95477f030c3a538
DonQuijoTeDe <q910553@gmail.com>
2015-12-06 15:18:23 +0800

    test almost done

--- test/test.js
+++ test/test.js
@@ -1,21 +1,56 @@
-describe('Test SignalChannelManager ', function () {
+describe('Test signalchannel ', function () {
     var subject;
 
     beforeEach(function () {
       subject = new SignalChannelManager();
     });
 
-    describe('Test register function', function() {
+    describe('Test register function', function () {
       it('test object', function () {
         subject.register(null);
         assert.equal(subject.login_name, '', "The test is fail!");
-        this.peer = ''
       });
-
       it('test object', function () {
         subject.register(null);
         assert.isNotNull(subject.peer, "The test is fail!");
       });
-    })
+    });
+
+    describe('Test call_connecting function', function () {
+      it('test object', function () {
+        subject.register(null);
+        subject.call_connecting();
+        assert.equal(subject.call_name, '', "The test is fail!");
+      });
+      it('test object', function () {
+        subject.register(null);
+        subject.call_connecting();
+        assert.isNotNull(subject.call, "The test is fail!");
+      });
+    });
+
+});
+
+describe('Test mediastream', function () {
+  var subject;
+
+  beforeEach(function () {
+    subject = new MediaStreamManager();
+  });
+
+  describe('Test start function', function () {
+    it('Test object' , function () {
+      subject.start(false);
+      assert.equal(subject.audio_set, false, "The test is fail!");
+    });
+    it('Test object' , function () {
+      subject.start(true);
+      assert.equal(subject.audio_set, true, "The test is fail!");
+    });
+    it('Test object' , function () {
+      subject.start(null);
+      assert.isNull(subject.audio_set, "The test is fail!");
+    });
+  });
 
 });
